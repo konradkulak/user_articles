37d7a4e1c2053b4e8b8a5d99169a90f645e12314
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-05-30 09:13:38 +0200

    Implement json serializable and freezed in author_model

--- lib/domain/models/article_model.dart
+++ lib/domain/models/article_model.dart
@@ -1,16 +1,36 @@
-class ArticleModel {
-  const ArticleModel({
-    required this.id,
-    required this.authorId,
-    required this.content,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  final int id;
-  final int authorId;
-  final String content;
 
-  ArticleModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        authorId = json['author_id'],
-        content = json['content'];
+part 'article_model.freezed.dart';
+part 'article_model.g.dart';
+
+@freezed
+class ArticleModel with _$ArticleModel {
+  factory ArticleModel(
+    int id,
+    @JsonKey(name: 'author_id') int authorId,
+    String content,
+  ) = _ArticleModel;
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) =>
+      _$ArticleModelFromJson(json);
 }
+
+// @JsonSerializable()
+// class ArticleModel {
+//   const ArticleModel({
+//     required this.id,
+//     required this.authorId,
+//     required this.content,
+//   });
+
+//   final int id;
+//   @JsonKey(name: 'author_id')
+//   final int authorId;
+//   final String content;
+
+//   factory ArticleModel.fromJson(Map<String, dynamic> json) =>
+//       _$ArticleModelFromJson(json);
+
+//   Map<String, dynamic> toJson() => _$ArticleModelToJson(this);
+// }
---
+++ lib/domain/models/article_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ArticleModelImpl _$$ArticleModelImplFromJson(Map<String, dynamic> json) =>
+    _$ArticleModelImpl(
+      (json['id'] as num).toInt(),
+      (json['author_id'] as num).toInt(),
+      json['content'] as String,
+    );
+
+Map<String, dynamic> _$$ArticleModelImplToJson(_$ArticleModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'author_id': instance.authorId,
+      'content': instance.content,
+    };
