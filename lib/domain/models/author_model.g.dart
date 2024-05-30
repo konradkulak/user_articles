10f086a9ab7b2ca734c0dfdd45c71f57d636249a
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-05-30 09:13:23 +0200

    Implement json serializable and freezed in author_model

---
+++ lib/domain/models/author_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'author_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AuthorModelImpl _$$AuthorModelImplFromJson(Map<String, dynamic> json) =>
+    _$AuthorModelImpl(
+      (json['id'] as num).toInt(),
+      json['picture'] as String,
+      json['first_name'] as String,
+      json['last_name'] as String,
+    );
+
+Map<String, dynamic> _$$AuthorModelImplToJson(_$AuthorModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'picture': instance.picture,
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+    };
