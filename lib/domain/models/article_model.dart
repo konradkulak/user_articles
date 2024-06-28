38549e0ade342e06d96376f361f2222b978a3906
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-06-28 16:12:13 +0200

    Remove @injectable annotation from all Cubit and Repository files

--- lib/domain/models/article_model.dart
+++ lib/domain/models/article_model.dart
@@ -1,14 +1,14 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-
 part 'article_model.freezed.dart';
 part 'article_model.g.dart';
 
 @freezed
 class ArticleModel with _$ArticleModel {
+  @JsonSerializable(fieldRename: FieldRename.snake)
   factory ArticleModel(
     int id,
-    @JsonKey(name: 'author_id') int authorId,
+    int authorId,
     String content,
   ) = _ArticleModel;
 
